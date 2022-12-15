a05c05f852f7d3c05aa517ad73bc4d2e63cdc5df
jonan270 <jonan270@student.liu.se>
2022-12-15 14:00:45 +0100

    Remove old comment

--- assets/gql/Query.js
+++ assets/gql/Query.js
@@ -1,6 +1,5 @@
 import { gql, useSubscription } from "@apollo/client";
 
-// Query from https://stackoverflow.com/questions/48244950/can-i-list-githubs-public-repositories-using-graphql
 export const GH_QUERY = gql`query GH_QUERY($searchParams:String!) {
     search(query: $searchParams, type: REPOSITORY, first: 50) {
         pageInfo {
